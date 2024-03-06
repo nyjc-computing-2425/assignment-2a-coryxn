f79d392797a31d8984cb31597eec14705531af9e
corinne <160467276+coryxn@users.noreply.github.com>
2024-03-06 09:08:59 +0000

    resubmission

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 num = input('Enter a number (decimal or integer): ')
 # type your code here
-
-
-
+num1 = num.replace('.', '')
+num2 = num1.lstrip('0')
+sf = len(num2)
 # do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
